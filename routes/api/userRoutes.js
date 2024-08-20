6943c1389980c1653dde731505399a0353b29d3f
mandi7469 <mandi7469@aol.com>
2024-08-20 18:49:43 -0400

    completed User routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const {
+  deleteUser,
+  updateUser,
+  getUsers,
+  getSingleUser,
+  createUser,
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/userController");
+
+// /api/users
+router.route("/").get(getUsers).post(createUser);
+
+// /api/users/:userId
+router
+.route("/:userId")
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser);
+
+// /api/users/:userId/friends/friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
+
+module.exports = router;
