6cb2f5807ba500adaa1739639520cd3797571132
mandi7469 <mandi7469@aol.com>
2024-08-21 16:47:14 -0400

    completed reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,24 @@
+const { Schema, Types } = require("mongoose");
+
+//schema only - used as the reaction field's subdocument schema in the Thought model
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = reactionSchema;
