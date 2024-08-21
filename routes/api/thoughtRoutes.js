9465953d2426ae70ccd7f6dabe367a7f9bd54105
mandi7469 <mandi7469@aol.com>
2024-08-21 17:14:05 -0400

    completed thought routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const {
+  deleteThought,
+  updateThought,
+  getThoughts,
+  getSingleThought,
+  createThought,
+  addReaction,
+  deleteReaction
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+.route('/:thoughtId')
+.get(getSingleThought)
+.put(updateThought)
+.delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
